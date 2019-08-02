2906de5a941df173284b6dc708f6e1d8ae1ebeff
wasimnawaz <wasimnawaz36@gmail.com>
2019-08-02 17:23:07 +0500

    Multiply Test

--- code-file.test.js
+++ code-file.test.js
@@ -4,7 +4,7 @@ const multiply = require('./code-file');
   expect(sum(1, 2)).toBe(3);
 }); */
 
-test('adds 1 + 2 to equal 3', sumTest)
+test('adds 1 + 2 to equal 3', multiplyTest)
 function multiplyTest() {
   expect(multiply(2, 2)).toBe(10);
 };
